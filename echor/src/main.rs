ebfa36a708a55e0768a2bcf51a0be5ca93b9add5
Antonio Feregrino <antonio.feregrino@gmail.com>
2025-06-08 09:12:29 +0100

    Add `clap` to `echor`

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -1,3 +1,26 @@
+use clap::{Arg, ArgAction, Command};
+
 fn main() {
-    println!("Hello, world!");
+    let program = Command::new("echor")
+        .version("0.1.0")
+        .author("Antonio Feregrino <antonio.feregrino@gmail.com>")
+        .about("A simple echo program");
+
+    let matches = program
+        .arg(
+            Arg::new("text")
+                .value_name("TEXT")
+                .help("The text to echo")
+                .required(true)
+                .num_args(1..),
+        )
+        .arg(
+            Arg::new("omit_newline")
+                .short('n')
+                .help("Do not print the trailing newline character")
+                .action(ArgAction::SetTrue),
+        )
+        .get_matches();
+
+    println!("{:?}", matches);
 }
