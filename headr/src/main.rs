1ea0b4690a01b46a0507b02d908cd4e797a5ee58
Antonio Feregrino <antonio.feregrino@gmail.com>
2025-06-08 12:31:25 +0100

    Cover case with lines

--- headr/src/main.rs
+++ headr/src/main.rs
@@ -1,20 +1,44 @@
+use anyhow::Result;
 use clap::Parser;
+use std::io::BufRead;
 
 #[derive(Parser, Debug)]
 #[command(version, author, about)]
 struct Args {
     #[arg(value_name="FILES", default_value="-", num_args=1..)]
     files: Vec<String>,
-    #[arg(value_name="LINES",short('n'), long("lines"), value_parser=clap::value_parser!(u64).range(1..))]
-    lines: Option<u64>,
+    #[arg(value_name="LINES",short('n'), long("lines"), default_value="10", value_parser=clap::value_parser!(u64).range(1..))]
+    lines: u64,
     #[arg(
         value_name="BYTES",
         short('c'),
-        long("bytes"), default_value="10", value_parser=clap::value_parser!(u64).range(1..), conflicts_with("lines"))]
-    bytes: u64,
+        long("bytes"), value_parser=clap::value_parser!(u64).range(1..), conflicts_with("lines"))]
+    bytes: Option<u64>,
+}
+
+fn run(args: Args) -> Result<()> {
+    let file_count = args.files.iter().count();
+    for filename in args.files {
+        let mut file = clir::open(&filename)?;
+        let mut line = String::new();
+        if file_count > 1 {
+            println!("==> {filename} <==")
+        }
+        for _ in 0..args.lines {
+            let read_bytes = file.read_line(&mut line)?;
+            if read_bytes == 0 {
+                break;
+            }
+            print!("{line}");
+            line.clear();
+        }
+    }
+    Ok(())
 }
 
 fn main() {
-    let args = Args::parse();
-    println!("{args:?}");
+    if let Err(err) = run(Args::parse()) {
+        eprintln!("{err}");
+        std::process::exit(1);
+    }
 }
