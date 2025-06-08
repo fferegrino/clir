0e7b57c39f2121e4c4def60d3de2430d1a2161c1
Antonio Feregrino <antonio.feregrino@gmail.com>
2025-06-08 09:34:36 +0100

    Change `echor` to use the `derive` pattern of `clap`

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -1,38 +1,22 @@
-use clap::{Arg, ArgAction, Command};
+use clap::ArgAction;
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(version, author, about)]
+struct Args {
+    #[arg(value_name="TEXT", help="The text to echo", required=true, num_args(1..))]
+    text: Vec<String>,
+    #[arg(short='n', help="Do not print the trailing newline character", action=ArgAction::SetTrue)]
+    omit_newline: bool,
+}
 
 fn main() {
-    let program = Command::new("echor")
-        .version("0.1.0")
-        .author("Antonio Feregrino <antonio.feregrino@gmail.com>")
-        .about("A simple echo program");
+    let args = Args::parse();
 
-    let matches = program
-        .arg(
-            Arg::new("text")
-                .value_name("TEXT")
-                .help("The text to echo")
-                .required(true)
-                .num_args(1..),
-        )
-        .arg(
-            Arg::new("omit_newline")
-                .short('n')
-                .help("Do not print the trailing newline character")
-                .action(ArgAction::SetTrue),
-        )
-        .get_matches();
-
-    let texts = matches
-        .get_many::<String>("text")
-        .unwrap_or_default()
-        .cloned()
-        .collect::<Vec<_>>();
-    let omit_newline = matches.get_flag("omit_newline");
-
-    let all_texts = texts.join(" ");
+    let all_texts = args.text.join(" ");
     print!("{}", all_texts);
 
-    if !omit_newline {
+    if !args.omit_newline {
         println!();
     }
 }
